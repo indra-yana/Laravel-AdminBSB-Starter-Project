ff9f2ab220fda8d72467e3baa976cfbcdb8e9656
Indra Muliana <43288611+indra-yana@users.noreply.github.com>
2024-03-07 08:16:35 +0700

    Update UsersTableSeeder.php

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
